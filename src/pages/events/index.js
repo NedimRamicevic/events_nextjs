2d550460894e4d70adc957c1ce29973a086f229c
Nedim <mr.ramicevic@gmail.com>
2023-04-04 12:09:01 +0300

    components

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,24 +1,16 @@
 // events main page
 import Image from "next/image";
+import Link from "next/link";
+import Events from "../../components/Events";
 
 function EventsPage({ data }) {
-  return (
-    <div>
-      <h1>Events Page</h1>
-      {data.map((item) => (
-        <a href={`/events/${item.id}`} key={item.id}>
-          <Image src={item.image} width={170} height={170} alt={item.title} />
-          <h2>{item.title}</h2>
-        </a>
-      ))}
-    </div>
-  );
+  return <Events data={data} />;
 }
 
 export default EventsPage;
 
 export async function getStaticProps() {
-  const { events_categories } = await import("../../../data/data.json");
+  const { events_categories } = await import("/data/data.json");
 
   return {
     props: {
